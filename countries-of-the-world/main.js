e29b0e49ce10e55ca7f1e510d7a96600b2feab7f
Martijn Doensen <martijn.doensen@live.nl>
2021-02-23 16:23:11 +0100

    Add new page: Countries of the World

---
+++ countries-of-the-world/main.js
@@ -0,0 +1,59 @@
+async function sortCountries() {
+	try {
+		const result = await axios.get('https://restcountries.eu/rest/v2/all');
+		const sortedCountries = result.data.sort(
+			(country1, country2) => country1.population - country2.population
+		);
+		for (country of sortedCountries) {
+			const { name, flag, population, region } = country;
+			makeList(name, flag, region);
+		}
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+function makeList(countryName, countryFlag, countryRegion) {
+	const list = document.querySelector('.list');
+	let country = document.createElement('li');
+	let name = document.createElement('span');
+	let flag = document.createElement('img');
+
+	name.classList.add('country-name');
+	flag.classList.add('country-flag');
+
+	name.textContent = countryName;
+	colorCodeCountry(name, countryRegion);
+	flag.setAttribute('src', countryFlag);
+
+	country.appendChild(flag);
+	country.appendChild(name);
+	list.appendChild(country);
+
+
+}
+
+function colorCodeCountry(nameElement, countryRegion) {
+	switch (countryRegion) {
+		case 'Africa':
+			nameElement.classList.add('africa');
+			break;
+		case 'Americas':
+			nameElement.classList.add('americas');
+			break;
+		case 'Asia':
+			nameElement.classList.add('asia');
+			break;
+		case 'Europe':
+			nameElement.classList.add('europe');
+			break;
+		case 'Oceania':
+			nameElement.classList.add('oceania');
+			break;
+		case 'Polar':
+			nameElement.classList.add('polar');
+			break;
+	}
+}
+
+sortCountries();
